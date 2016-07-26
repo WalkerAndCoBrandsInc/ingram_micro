d41f8e57ca1ddc970d7313081804ec184b857f8a
Khaaliq DeJan <khaaliqdejan@gmail.com>
2016-07-26 11:57:23 -0700

    IngramMicro::StandardResponse now takes in a transaction-name as an option
    - add more standard-response examples received from Brightpoint
    - move BPXML_InventoryStatus.xsd to the /xsd/inbound folder
    - move BPXML_StandardResponse.xsd to the /xsd/outbound folder
    - update standard response Fabricator and spec to account for changes
    - update Transmission to account for new XSD location
    
    [#127105987]

--- spec/fabricators/standard_response.rb
+++ spec/fabricators/standard_response.rb
@@ -1,4 +1,5 @@
 Fabricator(:standard_response, class_name: IngramMicro::StandardResponse) do
+  transaction_name { 'standard-response' }
   status_code { 1 }
   status_description { Faker::Lorem.sentence }
   comments { Faker::Lorem.sentence }
