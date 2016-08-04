3b937db92d4d1b3bdfae362286cb0d7960407570
Senthil <senthil@walkerandcobrands.com>
2016-08-04 11:21:52 -0700

    Unset 'customer_id' when building purchase order information

--- lib/ingram_micro/elements/outbound_purchase_order_information.rb
+++ lib/ingram_micro/elements/outbound_purchase_order_information.rb
@@ -1,5 +1,4 @@
 class IngramMicro::OutboundPurchaseOrderInformation < IngramMicro::BaseElement
-
   DEFAULTS = {
     purchase_order_number: nil,
     account_description: nil,
@@ -8,6 +7,13 @@ class IngramMicro::OutboundPurchaseOrderInformation < IngramMicro::BaseElement
     currency_code: nil,
     comments: nil
   }
+
+  def initialize(options = {})
+    super(options)
+
+    # customer_id is always set by BaseElement, but we don't need it here
+    self.element.delete(:customer_id)
+  end
 
   def defaults
     DEFAULTS
