e39075f0d51ac2a1308e759301a3f3f5f09d2ddf
Joseph Nguyen <joseph@walkerandcobrands.com>
2016-08-08 17:13:29 -0700

    WIP set up gem to process inventory synchronization

--- lib/ingram_micro/inbound_elements/inventory_sync.rb
+++ lib/ingram_micro/inbound_elements/inventory_sync.rb
@@ -1,2 +1,15 @@
 class IngramMicro::InventorySync < IngramMicro::InboundBaseElement
+
+  def message_header
+    @hash['message']['message_header']
+  end
+
+  def inventory_status
+    @hash['message']['inventory_synchronization']
+  end
+
+  def details
+    @hash['message']['inventory_synchronization']['details']['line_item']
+  end
+
 end
