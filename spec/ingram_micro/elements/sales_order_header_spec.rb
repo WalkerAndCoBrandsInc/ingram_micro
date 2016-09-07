1eb3e191d3b1acfed8928cff9c5cf01bb74e1f1b
Joseph Nguyen <joseph@walkerandcobrands.com>
2016-09-07 11:53:27 -0700

    Improve SalesOrderHeader to accommodate header-name-value element
    - update #build to allow passing in of header-name-value attributes
    - add comments
    - update specs
    
    [#129158777]

--- spec/ingram_micro/elements/sales_order_header_spec.rb
+++ spec/ingram_micro/elements/sales_order_header_spec.rb
@@ -1,37 +1,61 @@
 require 'spec_helper'
 
 describe IngramMicro::SalesOrderHeader do
-  it 'formats the customer_order_date as YYYYMMDD' do
-    Nokogiri::XML::Builder.new do |builder|
-      builder.send('message') do
-        IngramMicro::SalesOrderHeader.new(
+  describe "#build" do
+    it 'formats the customer_order_date as YYYYMMDD' do
+      Nokogiri::XML::Builder.new do |builder|
+        builder.send('message') do
+          IngramMicro::SalesOrderHeader.new(
           customer_order_date: Time.new(2016, 6, 15)
-        ).build(builder)
+          ).build(builder)
+        end
+
+        expect(builder.to_xml).to include('<customer-order-date>20160615</customer-order-date>')
       end
-
-      expect(builder.to_xml).to include('<customer-order-date>20160615</customer-order-date>')
     end
-  end
 
-  it 'defaults the customer_order_date to empty if it is not passed' do
-    Nokogiri::XML::Builder.new do |builder|
-      builder.send('message') do
-        IngramMicro::SalesOrderHeader.new.build(builder)
+    it 'defaults the customer_order_date to empty if it is not passed' do
+      Nokogiri::XML::Builder.new do |builder|
+        builder.send('message') do
+          IngramMicro::SalesOrderHeader.new.build(builder)
+        end
+
+        expect(builder.to_xml).to include('<customer-order-date/>')
       end
-
-      expect(builder.to_xml).to include('<customer-order-date/>')
     end
-  end
 
-  it 'also accepts a string for customer_order_date' do
-    Nokogiri::XML::Builder.new do |builder|
-      builder.send('message') do
-        IngramMicro::SalesOrderHeader.new(
+    it 'also accepts a string for customer_order_date' do
+      Nokogiri::XML::Builder.new do |builder|
+        builder.send('message') do
+          IngramMicro::SalesOrderHeader.new(
           customer_order_date: '20160615'
-        ).build(builder)
+          ).build(builder)
+        end
+
+        expect(builder.to_xml).to include('<customer-order-date>20160615</customer-order-date>')
+      end
+    end
+
+
+
+  end
+
+  describe "#add_header_name_values" do
+    it 'allows header-name-value to be passed in' do
+      soh_options = {
+        customer_order_number: 2083648614,
+        order_total_net: 39.46,
+        header_name_values: [["international-importer-of-record-email", "email@canada.ca"]]
+      }
+      soh_builder = Nokogiri::XML::Builder.new do |builder|
+        builder.send('message') do
+          IngramMicro::SalesOrderHeader.new(soh_options).build(builder)
+        end
       end
 
-      expect(builder.to_xml).to include('<customer-order-date>20160615</customer-order-date>')
+      expect(soh_builder.to_xml).to include('<header-name-value>')
+      expect(soh_builder.to_xml).to include('<attribute-name>international-importer-of-record-email')
+      expect(soh_builder.to_xml).to include('<attribute-value>email@canada.ca')
     end
   end
 end
