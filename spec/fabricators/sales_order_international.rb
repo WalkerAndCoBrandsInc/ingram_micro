0d818be37365b10730d5a6f49ce9be81ee2ad7e4
vietjew <vietjew@gmail.com>
2016-09-13 10:19:05 -0700

    Refactor and add new Fabricator to work with international shipping
    - fix SalesOrderLineItem to add the line-name-value attributes correctly
    - add Fabricator for SalesOrderLineItemInternational
    - update SalesOrderInternational Fabricator

--- spec/fabricators/sales_order_international.rb
+++ spec/fabricators/sales_order_international.rb
@@ -4,7 +4,7 @@ Fabricator(:sales_order_international, class_name: IngramMicro::SalesOrderIntern
   customer_id { (0..100).to_a.sample }
   customer { Fabricate.build(:customer) }
   credit_card_information { Fabricate.build(:credit_card_information) }
-  sales_order_header { Fabricate.build(:sales_order_header_international)}
+  sales_order_header { Fabricate.build(:sales_order_header_international) }
   sales_order_shipment_information { Fabricate.build(:sales_order_shipment_information) }
-  detail { IngramMicro::Detail.new(line_items: [Fabricate.build(:sales_order_line_item)])}
+  detail { IngramMicro::Detail.new(line_items: [Fabricate.build(:sales_order_line_item_international)])}
 end
