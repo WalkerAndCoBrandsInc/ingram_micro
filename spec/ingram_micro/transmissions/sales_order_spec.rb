fb2a8fc0a2e89a06882f1e0358e25fb98f3e72fd
David Le <david@walkerandcobrands.com>
2017-10-13 14:48:26 -0400

    Pass in customer ID as a possible parameter

--- spec/ingram_micro/transmissions/sales_order_spec.rb
+++ spec/ingram_micro/transmissions/sales_order_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe IngramMicro::SalesOrder do
   let(:empty_sales_order) do
@@ -7,41 +7,57 @@ describe IngramMicro::SalesOrder do
 
   let(:populated_sales_order) { Fabricate.build(:sales_order) }
 
-  describe '#initialize' do
-    context 'with no data passed in' do
-      it 'should still create a valid SalesOrder object' do
+  describe "#initialize" do
+    context "with no data passed in" do
+      it "should still create a valid SalesOrder object" do
         expect(empty_sales_order).to be_truthy
       end
     end
-    context 'with data passed in' do
-      it 'should create a valid SalesOrder object with the data passed in' do
+    context "with data passed in" do
+      it "should create a valid SalesOrder object with the data passed in" do
         expect(populated_sales_order).to be_truthy
       end
     end
-    context 'with sales order shipment information' do
+    context "with sales order shipment information" do
       let(:sales_order_shipment_information) { Fabricate.build(:sales_order_shipment_information) }
-      it 'sets the attribute' do
+      it "sets the attribute" do
         sales_order = IngramMicro::SalesOrder.new({sales_order_shipment_information: sales_order_shipment_information})
         expect(sales_order.sales_order_shipment_information).to eq(sales_order_shipment_information)
       end
     end
 
-    context 'with purchase order information' do
+    context "with purchase order information" do
       let(:sales_order_purchase_order_information) { Fabricate.build(:sales_order_purchase_order_information) }
-      it 'sets the attribute' do
+      it "sets the attribute" do
         sales_order = IngramMicro::SalesOrder.new({purchase_order_information: sales_order_purchase_order_information})
         expect(sales_order.purchase_order_information).to eq(sales_order_purchase_order_information)
       end
     end
 
-    context 'old options passed in' do
-      it 'raises for shipment information' do
+    describe "customer id attribute" do
+      context "with customer id param passed in" do
+        it "sets customer_id" do
+          sales_order = IngramMicro::SalesOrder.new({customer_id: 12341234})
+          expect(sales_order.customer_id).to eq(12341234)
+        end
+      end
+
+      context "with customer id param not passed in" do
+        it "pulls from the configuration" do
+          sales_order = IngramMicro::SalesOrder.new()
+          expect(sales_order.customer_id).to eq(123)
+        end
+      end
+    end
+
+    context "old options passed in" do
+      it "raises for shipment information" do
         expect do
           IngramMicro::SalesOrder.new({shipment_information: Fabricate.build(:sales_order_shipment_information)})
         end.to raise_error(/sales_order_shipment_information/)
       end
 
-      it 'raises for order header' do
+      it "raises for order header" do
         expect do
           IngramMicro::SalesOrder.new({order_header: Fabricate.build(:sales_order_header)})
         end.to raise_error(/sales_order_header/)
@@ -55,37 +71,37 @@ describe IngramMicro::SalesOrder do
       IngramMicro.configure { |config| config.international_schema = false }
     end
 
-    describe '#schema_valid?' do
-      context 'with data passed in' do
-        it 'validates output xml using SalesOrder schema' do
+    describe "#schema_valid?" do
+      context "with data passed in" do
+        it "validates output xml using SalesOrder schema" do
           expect(populated_sales_order.schema_valid?).to be true
         end
       end
     end
 
-    describe 'xml_builder' do
-      context 'populated sales order' do
-        it 'generates xml' do
-          expect(populated_sales_order.xml_builder.to_xml).to have_xml('/message/message-header/transaction-name', 'sales-order-submission')
+    describe "xml_builder" do
+      context "populated sales order" do
+        it "generates xml" do
+          expect(populated_sales_order.xml_builder.to_xml).to have_xml("/message/message-header/transaction-name", "sales-order-submission")
         end
       end
 
-      context 'passed shipment information' do
+      context "passed shipment information" do
         let(:ship_first_name) { populated_sales_order.sales_order_shipment_information.element[:ship_first_name] }
         before do
           expect(ship_first_name).to_not be_nil
         end
 
-        it 'includes it in xml' do
-          expect(populated_sales_order.xml_builder.to_xml).to have_xml('/message/sales-order-submission/header/shipment-information/ship-first-name', ship_first_name)
+        it "includes it in xml" do
+          expect(populated_sales_order.xml_builder.to_xml).to have_xml("/message/sales-order-submission/header/shipment-information/ship-first-name", ship_first_name)
         end
       end
     end
 
-    describe 'order_builder' do
-      it 'warns for deprecation and calls xml_builder' do
-        expect(populated_sales_order).to receive(:warn).with('[DEPRECATION] `order_builder` is deprecated.  Please use `xml_builder` instead.')
-          expect(populated_sales_order.order_builder.to_xml).to have_xml('/message/message-header/transaction-name', 'sales-order-submission')
+    describe "order_builder" do
+      it "warns for deprecation and calls xml_builder" do
+        expect(populated_sales_order).to receive(:warn).with("[DEPRECATION] `order_builder` is deprecated.  Please use `xml_builder` instead.")
+          expect(populated_sales_order.order_builder.to_xml).to have_xml("/message/message-header/transaction-name", "sales-order-submission")
       end
     end
   end
@@ -98,37 +114,37 @@ describe IngramMicro::SalesOrder do
 
     let!(:international_sales_order) { Fabricate.build(:sales_order_international) }
 
-    describe '#schema_valid?' do
-      context 'with data passed in' do
-        it 'validates output xml using SalesOrder schema' do
+    describe "#schema_valid?" do
+      context "with data passed in" do
+        it "validates output xml using SalesOrder schema" do
           expect(international_sales_order.schema_valid?).to be true
         end
       end
     end
 
-    describe 'xml_builder' do
-      context 'populated sales order' do
-        it 'generates xml' do
-          expect(international_sales_order.xml_builder.to_xml).to have_xml('/message/message-header/transaction-name', 'sales-order-submission')
+    describe "xml_builder" do
+      context "populated sales order" do
+        it "generates xml" do
+          expect(international_sales_order.xml_builder.to_xml).to have_xml("/message/message-header/transaction-name", "sales-order-submission")
         end
       end
 
-      context 'passed shipment information' do
+      context "passed shipment information" do
         let(:ship_first_name) { international_sales_order.sales_order_shipment_information.element[:ship_first_name] }
         before do
           expect(ship_first_name).to_not be_nil
         end
 
-        it 'includes it in xml' do
-          expect(international_sales_order.xml_builder.to_xml).to have_xml('/message/sales-order-submission/header/shipment-information/ship-first-name', ship_first_name)
+        it "includes it in xml" do
+          expect(international_sales_order.xml_builder.to_xml).to have_xml("/message/sales-order-submission/header/shipment-information/ship-first-name", ship_first_name)
         end
       end
     end
 
-    describe 'order_builder' do
-      it 'warns for deprecation and calls xml_builder' do
-        expect(international_sales_order).to receive(:warn).with('[DEPRECATION] `order_builder` is deprecated.  Please use `xml_builder` instead.')
-          expect(international_sales_order.order_builder.to_xml).to have_xml('/message/message-header/transaction-name', 'sales-order-submission')
+    describe "order_builder" do
+      it "warns for deprecation and calls xml_builder" do
+        expect(international_sales_order).to receive(:warn).with("[DEPRECATION] `order_builder` is deprecated.  Please use `xml_builder` instead.")
+          expect(international_sales_order.order_builder.to_xml).to have_xml("/message/message-header/transaction-name", "sales-order-submission")
       end
     end
   end
